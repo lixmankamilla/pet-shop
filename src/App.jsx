04a84c679c06b13ab16caae626fd574216f6160d
Kamilla <kamila.lixmanova@kapitalbank.az>
2025-01-21 15:43:33 +0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+import CartPage from "./pages/CartPage";
+import ContactPage from "./pages/ContactPage";
+import Navbar from "./components/Navbar";
+import "./styles/index.scss";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
